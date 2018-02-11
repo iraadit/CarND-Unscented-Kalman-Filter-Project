e91ab9b618cb6af7508f6b005c349e8a2e0bab9a
Renaud Bouckenooghe <iraadit@gmail.com>
2018-02-11 23:07:16 +0100

    fix: caculate only one time cos and sin of yaw and delta_t squared
    "code smell"

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -198,6 +198,8 @@ void UKF::Prediction(double delta_t) {
   /**
   * Calculate Augmented Sigma Points
   */
+  double delta_t_2 = delta_t * delta_t;
+
   //create augmented mean vector
   VectorXd x_aug = VectorXd(n_aug_);
 
@@ -248,27 +250,30 @@ void UKF::Prediction(double delta_t) {
     //predicted state values
     double px_p, py_p;
 
+    double sin_yaw = sin(yaw);
+    double cos_yaw = cos(yaw);
+
     //avoid division by zero
     if (fabs(yawd) > 0.001) {
-        px_p = p_x + v/yawd * ( sin (yaw + yawd*delta_t) - sin(yaw));
-        py_p = p_y + v/yawd * ( cos(yaw) - cos(yaw+yawd*delta_t) );
+        px_p = p_x + v / yawd * ( sin (yaw + yawd * delta_t) - sin_yaw);
+        py_p = p_y + v / yawd * ( cos_yaw - cos(yaw + yawd * delta_t) );
     }
     else {
-        px_p = p_x + v*delta_t*cos(yaw);
-        py_p = p_y + v*delta_t*sin(yaw);
+        px_p = p_x + v * delta_t * cos_yaw;
+        py_p = p_y + v * delta_t * sin_yaw;
     }
 
     double v_p = v;
-    double yaw_p = yaw + yawd*delta_t;
+    double yaw_p = yaw + yawd * delta_t;
     double yawd_p = yawd;
 
     //add noise
-    px_p = px_p + 0.5*nu_a*delta_t*delta_t * cos(yaw);
-    py_p = py_p + 0.5*nu_a*delta_t*delta_t * sin(yaw);
-    v_p = v_p + nu_a*delta_t;
+    px_p = px_p + 0.5 * nu_a * delta_t_2 * cos_yaw;
+    py_p = py_p + 0.5 * nu_a * delta_t_2 * sin_yaw;
+    v_p = v_p + nu_a * delta_t;
 
-    yaw_p = yaw_p + 0.5*nu_yawdd*delta_t*delta_t;
-    yawd_p = yawd_p + nu_yawdd*delta_t;
+    yaw_p = yaw_p + 0.5 * nu_yawdd * delta_t_2;
+    yawd_p = yawd_p + nu_yawdd * delta_t;
 
     //write predicted sigma point into right column
     Xsig_pred_(0,i) = px_p;
